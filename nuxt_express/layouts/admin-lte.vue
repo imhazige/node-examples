e05e51e5e73036f13ffe566358f30f92d2f77c4e
Ka <imhazige@gmail.com>
2018-04-10 15:28:12 +0800

    font did not show correctly

--- nuxt_express/layouts/admin-lte.vue
+++ nuxt_express/layouts/admin-lte.vue
@@ -1,16 +1,8 @@
 <template>
     <div class="wrapper">
-          <header class="main-header">
-            <!-- Logo -->
-            <a href="../../index2.html" class="logo">
-            <!-- mini logo for sidebar mini 50x50 pixels -->
-            <span class="logo-mini"><b>A</b>LT</span>
-            <!-- logo for regular state and mobile devices -->
-            <span class="logo-lg"><b>Admin</b>LTE</span>
-            </a>
-
+          <Header class="main-header">
             <NavBar />
-          </header>
+          </Header>
 
           <SideBar />  
           <nuxt/>
@@ -20,12 +12,14 @@
 </template>
 
 <script>
+import Header from "~/components/Header.vue";
 import NavBar from "~/components/NavBar.vue";
 import SideBar from "~/components/SideBar.vue";
 import Footer from "~/components/Footer.vue";
 
 export default {
   components: {
+    Header,
     NavBar,
     SideBar,
     Footer
@@ -44,7 +38,13 @@ export default {
       ],
       //you have to specify the absolute path(or in the static folder) of the css in this way
       //see https://nuxtjs.org/guide/assets
-      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+      link: [
+        { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+        // {
+        //   rel: "stylesheet",
+        //   href: "https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic"
+        // }
+      ],
       //see https://github.com/nuxt/nuxt.js/issues/188
       bodyAttrs: {
         class: "hold-transition skin-blue sidebar-mini"
@@ -58,3 +58,12 @@ export default {
 <style src="admin-lte/dist/css/AdminLTE.css"></style>
 <style src="admin-lte/dist/css/skins/skin-blue.min.css"></style>
 <style src="~/assets/css/googlefont1.css"></style>
+<style>
+/* to set style for the additional wrapper element created by nuxt */
+#__nuxt {
+  height: 100%;
+}
+#__layout {
+  height: 100%;
+}
+</style>
