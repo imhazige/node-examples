5d04f16077949dd85b55c10a7973f0db8d22b4ee
Ka <imhazige@gmail.com>
2018-08-01 16:55:14 +0800

    add comment for a react test

--- meteor/client/routes.js
+++ meteor/client/routes.js
@@ -22,12 +22,25 @@ FlowRouter.route('/login/native', {
   }
 });
 
+/**
+ * this is not a correct way. look at the layout, we should not mix layout and react reander.
+ * because this way works only when user route from the page which layout have been loaded.
+ * thik about it, if go to this url directly, it will not have the layout loaded, and there will be no
+ * container, the #app element have not loaded yet.
+ * so if use blazelayout, alway make it the the top render mecanism, and make the react component as a
+ * child of the layout
+ *
+ */
 FlowRouter.route('/test/reactrender', {
   name: 'Test.ReactRender',
   action(params, queryParams) {
     // BlazeLayout.render('My_Content', { main: 'Login.UIAccountsBootstrap' });
-    COMP = require('./HelloWorld').default;
-    document.title = '哈哈';
-    render(<COMP />, document.getElementById('app'));
+    import('./HelloWorld').then(m => {
+      const COMP = m.default;
+      document.title = '哈哈';
+
+      render(<COMP />, document.getElementById('app'));
+    });
+    // COMP = require('./HelloWorld').default;
   }
 });
