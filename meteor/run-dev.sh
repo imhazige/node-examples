a5aa3c61958d760e64049b0d585f257c42665c79
Ka <imhazige@gmail.com>
2018-08-01 17:51:10 +0800

    remove accounts-ui and password service, so need not mongo:next:apollo

--- meteor/run-dev.sh
+++ meteor/run-dev.sh
@@ -1,5 +1,5 @@
-# export MONGO_URL=false
-export MONGO_URL=mongodb://localhost:27017/meteor-test
+export MONGO_URL=false
+# export MONGO_URL=mongodb://localhost:27017/meteor-test
 
 METEOR="C:\Users\Administrator\AppData\Local\.meteor\meteor.bat"
 
