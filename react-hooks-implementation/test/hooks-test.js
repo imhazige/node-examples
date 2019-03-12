c932dbbd0dc0719207a944806d09299168f08bef
k.z <>
2019-03-12 19:09:42 +0800

    update

--- react-hooks-implementation/test/hooks-test.js
+++ react-hooks-implementation/test/hooks-test.js
@@ -1,9 +1,45 @@
 import assert from 'assert';
 import { describe, it } from 'mocha';
 import log from '../../common/log';
+import { React } from '..';
 
 describe('Hooks', () => {
   describe('test all ()', () => {
     it('test all', () => {});
+
+    function Counter() {
+      const [count, setCount] = React.useState(0);
+      const [text, setText] = React.useState(foo);
+      React.useEffect(() => {
+        console.log('effect', count, text);
+      }, [count, text]);
+      return {
+        click: () => setCount(count + 1),
+        type: txt => setText(txt),
+        noop: () => setCount(count),
+        render: () => console.log({ count, text }),
+      };
+    }
+    let App;
+    App = React.render(Counter);
+    // effect foo
+    // {count: 0, text: 'foo'}
+    App.click();
+    App = React.render(Counter);
+    // effect 1 foo
+    // {count: 1, text: 'foo'}
+    App.type('bar');
+    App = React.render(Counter);
+    // effect 1 bar
+    // {count: I, text: • bar ' }
+    App.noop();
+    App = React.render(Counter);
+    // // no effect run
+    // effect 1 bar
+    // {count: 1, text: 'bar'}
+    App.click();
+    App = React.render(Counter);
+    // effect 2 bar
+    // {count: 2, text: • bar ' }
   });
 });
